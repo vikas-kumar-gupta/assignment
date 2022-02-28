554eb97d56c8be7c34644c0423257e8027c447fa
vikas-kumar-gupta <vikas.vg118@gmail.com>
2022-02-28 13:46:10 +0530

    put + joi updated

--- controller/users.js
+++ controller/users.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const { use } = require("express/lib/router");
 
 let userJSONFile = path.resolve(__dirname, "../userData.json");
 let userData_Buffer = fs.readFileSync(userJSONFile);
@@ -50,29 +51,53 @@ let deleteUserData = (req, res) => {
       } else {
         res.send("data deleted successfully");
       }
-    })
+    });
   } else {
     res.send(`There is nothing data on ${userId} ID`);
   }
 };
 
 // update all tthe data of user using user id
-let updateAllUserData = (req, res) => {};
+let updateAllUserData = (req, res) => {
+  const userId = req.params.id;
+  const index = userData_JSON.users.findIndex((data) => data.id == userId);
+  if (index !== -1) {
+    const { id, name, email, contact, age, address } = req.body;
+    userData_JSON.users[index].name = name;
+    userData_JSON.users[index].email = email;
+    userData_JSON.users[index].id = id;
+    userData_JSON.users[index].contact = contact;
+    userData_JSON.users[index].age = age;
+    userData_JSON.users[index].address = address;
+    fs.writeFile(userJSONFile, JSON.stringify(userData_JSON), (err) => {
+      if (err) {
+        console.log("Something error occurred");
+      } else {
+        res.send("data updates successfully");
+      }
+    });
+  } else {
+    res.send(`There is nothing data on ${userId} ID`);
+  }
+};
 
 // update few data of the user using user id
 let updateFewUserData = (req, res) => {
   const userId = req.params.id;
   const index = userData_JSON.users.findIndex((data) => data.id == userId);
-  console.log(index);
-  userData_JSON.users[index] = req.body;
-  console.log(req.body);
-  fs.writeFile(userJSONFile, JSON.stringify(userData_JSON), (err) => {
-    if(err) {
-      console.log("Something error occurred");
-    } else {
-      res.send('data updates successfully')
-    }
-  })
+  if (index !== -1) {
+    userData_JSON.users[index] = req.body;
+    console.log(req.body);
+    fs.writeFile(userJSONFile, JSON.stringify(userData_JSON), (err) => {
+      if (err) {
+        console.log("Something error occurred");
+      } else {
+        res.send("data updates successfully");
+      }
+    });
+  } else {
+    res.send(`There is nothing data on ${userId} ID`);
+  }
 };
 
 module.exports.userController = {
